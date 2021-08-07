61afc8da3d07a0810d5721ee2bb1ffe99e635096
ms <shaheed_manzur@yahoo.com>
2021-08-06 22:38:58 -0400

    mongo db string

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // DB connect
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
