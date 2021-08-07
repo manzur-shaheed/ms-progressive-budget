05fd9711ab0b69b3e4174f8db18588645ed0c683
ms <shaheed_manzur@yahoo.com>
2021-08-06 22:38:21 -0400

    fix service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -41,23 +41,39 @@ self.addEventListener('activate', (event) => {
   );
 });
 
-// fetch from cache
-self.addEventListener('fetch', (event) => {
-  if (event.request.url.startsWith(self.location.origin)) {
+// fetch from cache first
+self.addEventListener("fetch", event => {
+  // handle runtime GET requests for data from /api routes
+  if (event.request.url.includes("/api")) {
+    // make network request and fallback to cache if network request fails (offline)
     event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return caches.open(RUNTIME).then((cache) => {
-          return fetch(event.request).then((response) => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
+      caches.open(RUNTIME).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            cache.put(event.request, response.clone());
+            return response;
+          })
+          .catch(() => caches.match(event.request));
       })
     );
+    return;
   }
+
+  // use cache first for all other requests for performance
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      // request is not in cache. make network request and cache the response
+      return caches.open(RUNTIME).then(cache => {
+        return fetch(event.request).then(response => {
+          return cache.put(event.request, response.clone()).then(() => {
+            return response;
+          });
+        });
+      });
+    })
+  );
 });
